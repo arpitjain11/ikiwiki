e910e67424aeaf56c547578af6c98914a7180811
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-03-24 02:42:19 +0000

    fixes

--- IkiWiki/Setup.pm
+++ IkiWiki/Setup.pm
@@ -9,8 +9,11 @@ sub setup () { # {{{
 	my $setup=possibly_foolish_untaint($config{setup});
 	delete $config{setup};
 	open (IN, $setup) || error("read $setup: $!\n");
-	local $/=undef;
-	my $code=<IN>;
+	my $code;
+	{
+		local $/=undef;
+		$code=<IN>;
+	}
 	($code)=$code=~/(.*)/s;
 	close IN;
 
