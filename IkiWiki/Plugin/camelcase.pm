3a2cb0ecfff65f728569edbdcc4e30f027c36d67
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-07-26 22:09:55 +0000

    * Fix CamelCase regexp to not break [[FooBar|BazBar]] WikiLinks.

--- IkiWiki/Plugin/camelcase.pm
+++ IkiWiki/Plugin/camelcase.pm
@@ -14,7 +14,7 @@ sub filter (@) { #{{{
 
 	# Make CamelCase links work by promoting them to fullfledged
 	# WikiLinks. This regexp is based on the one in Text::WikiFormat.
-	$params{content}=~s#(?<![["/>=])\b((?:[A-Z][a-z0-9]\w*){2,})#[[$1]]#g;
+	$params{content}=~s#(?<![[|"/>=])\b((?:[A-Z][a-z0-9]\w*){2,})#[[$1]]#g;
 
 	return $params{content};
 } #}}}
