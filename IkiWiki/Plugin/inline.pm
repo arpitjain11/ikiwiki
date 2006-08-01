53d81ebcd6cf235ae7db636b946f5342b863cc74
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-08-01 20:08:42 +0000

    * Patch from Roland Mas to support an rss=no parameter to inline directives.   Closes: #380743

--- IkiWiki/Plugin/inline.pm
+++ IkiWiki/Plugin/inline.pm
@@ -35,6 +35,9 @@ sub preprocess_inline (@) { #{{{
 	if (! exists $params{show} && $params{archive} eq "no") {
 		$params{show}=10;
 	}
+	if (! exists $params{rss}) {
+		$params{rss}="yes";
+	}
 
 	# Avoid nested inlines, to avoid loops etc.
 	if ($processing_inline) {
@@ -68,7 +71,7 @@ sub preprocess_inline (@) { #{{{
 		}
 		$ret.=$formtemplate->output;
 	}
-	elsif ($config{rss}) {
+	elsif ($config{rss} && $params{rss} eq "yes")) {
 		# Add a rss link button.
 		my $linktemplate=template("rsslink.tmpl", blind_cache => 1);
 		$linktemplate->param(rssurl => rsspage(basename($params{page})));
@@ -105,7 +108,7 @@ sub preprocess_inline (@) { #{{{
 	# TODO: should really add this to renderedfiles and call
 	# check_overwrite, but currently renderedfiles
 	# only supports listing one file per page.
-	if ($config{rss}) {
+	if ($config{rss} && $params{rss} eq "yes") {
 		writefile(rsspage($params{page}), $config{destdir},
 			genrss($params{page}, @list));
 		$toping{$params{page}}=1 unless $config{rebuild};
