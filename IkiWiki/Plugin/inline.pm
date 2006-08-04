d3713b01649a834c8977d387cddb02c24a3fa2d5
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-08-04 01:57:32 +0000

    * Encode link and guid urls in rss feeds to avoid illegal utf8 slipping in.

--- IkiWiki/Plugin/inline.pm
+++ IkiWiki/Plugin/inline.pm
@@ -5,6 +5,7 @@ package IkiWiki::Plugin::inline;
 use warnings;
 use strict;
 use IkiWiki;
+use HTML::Entities q{encode_entities_numeric};
 
 sub import { #{{{
 	IkiWiki::hook(type => "preprocess", id => "inline", 
@@ -162,7 +163,7 @@ sub genrss ($@) { #{{{
 	my $page=shift;
 	my @pages=@_;
 	
-	my $url="$config{url}/".htmlpage($page);
+	my $url=encode_entities_numeric("$config{url}/".htmlpage($page));
 	
 	my $itemtemplate=template("rssitem.tmpl", blind_cache => 1, 
 		die_on_bad_params => 0);
@@ -170,10 +171,12 @@ sub genrss ($@) { #{{{
 	foreach my $p (@pages) {
 		next unless exists $renderedfiles{$p};
 
+		my $u=encode_entities_numeric("$config{url}/$renderedfiles{$p}");
+
 		$itemtemplate->param(
 			title => pagetitle(basename($p)),
-			url => "$config{url}/$renderedfiles{$p}",
-			permalink => "$config{url}/$renderedfiles{$p}",
+			url => $u,
+			permalink => $u,
 			pubdate => date_822($pagectime{$p}),
 			content => absolute_urls(get_inline_content($p, $page), $url),
 		);
