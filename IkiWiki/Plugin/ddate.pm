a61d531edbe89f7677e8a2b2f1b82b189782dcdf
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-08-27 03:55:45 +0000

    * Added a ddate plugin.

---
+++ IkiWiki/Plugin/ddate.pm
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+# Discordian date support fnord ikiwiki.
+package IkiWiki::Plugin::ddate;
+use IkiWiki;
+use IkiWiki::Render; # so we can redefine it here:
+no warnings;
+
+sub import { #{{{
+	IkiWiki::hook(type => "checkconfig", id => "skeleton", 
+		call => \&checkconfig);
+} # }}}
+
+sub checkconfig () { #{{{
+	if (! defined $IkiWiki::config{timeformat} ||
+	    $IkiWiki::config{timeformat} eq '%c') {
+		$IkiWiki::config{timeformat}='on %{%A, the %e of %B%}, %Y. %N%nCelebrate %H';
+	}
+} #}}}
+
+sub IkiWiki::displaytime ($) { #{{{
+	my $time=shift;
+        eval q{use POSIX};
+        my $gregorian=POSIX::strftime("%d %m %Y", localtime($time));
+	my $date=`ddate +'$IkiWiki::config{timeformat}' $gregorian`;
+	chomp $date;
+	return $date;
+} #}}}
+
+5
