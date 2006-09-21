c7140ad90b0db2078e70b9cd3f08e1d176125191
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-09-21 20:28:40 +0000

    * Add typography (SmartyPants) plugin by Recai.

---
+++ IkiWiki/Plugin/typography.pm
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+package IkiWiki::Plugin::typography;
+
+use warnings;
+use strict;
+use IkiWiki;
+
+sub import { #{{{
+	IkiWiki::hook(type => "sanitize", id => "typography", call => \&sanitize);
+} # }}}
+
+sub sanitize (@) { #{{{
+	my %params=@_;
+
+	eval q{use Text::Typography};
+	return Text::Typography::typography($params{content});
+} # }}}
+
+1
