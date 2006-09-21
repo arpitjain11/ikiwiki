bbfe1b9058f3e2b73d6c8c1f7eed80d72727d2a4
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-09-21 20:30:03 +0000

    move use

--- IkiWiki/Plugin/typography.pm
+++ IkiWiki/Plugin/typography.pm
@@ -5,6 +5,7 @@ package IkiWiki::Plugin::typography;
 use warnings;
 use strict;
 use IkiWiki;
+use Text::Typography;
 
 sub import { #{{{
 	IkiWiki::hook(type => "sanitize", id => "typography", call => \&sanitize);
@@ -13,7 +14,6 @@ sub import { #{{{
 sub sanitize (@) { #{{{
 	my %params=@_;
 
-	eval q{use Text::Typography};
 	return Text::Typography::typography($params{content});
 } # }}}
 
