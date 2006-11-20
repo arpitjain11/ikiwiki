b6116256c513daffe91d55a34dc134c8a43f9c8f
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-11-20 10:29:39 +0000

    get rid of wikiformat build dep

--- IkiWiki/Plugin/wikitext.pm
+++ IkiWiki/Plugin/wikitext.pm
@@ -5,7 +5,6 @@ package IkiWiki::Plugin::wikitext;
 use warnings;
 use strict;
 use IkiWiki;
-use Text::WikiFormat;
 
 sub import { #{{{
 	hook(type => "htmlize", id => "wiki", call => \&htmlize);
@@ -15,6 +14,8 @@ sub htmlize (@) { #{{{
 	my %params=@_;
 	my $content = $params{content};
 
+	eval q{use Text::WikiFormat};
+	return $content if $@;
 	return Text::WikiFormat::format($content, undef, { implicit_links => 0 });
 } # }}}
 
