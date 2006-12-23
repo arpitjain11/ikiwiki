d83d11e6b87e718872e72153ee6bb3a382615c25
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2006-12-23 01:07:11 +0000

    * Add mirrorlist plugin.

---
+++ IkiWiki/Plugin/mirrorlist.pm
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+package IkiWiki::Plugin::mirrorlist;
+
+use warnings;
+use strict;
+use IkiWiki;
+
+sub import { #{{{
+	hook(type => "pagetemplate", id => "mirrorlist", call => \&pagetemplate);
+} # }}}
+
+sub pagetemplate (@) { #{{{
+	my %params=@_;
+        my $template=$params{template};
+	
+	$template->param(extrafooter => mirrorlist($params{page}))
+		if $template->query(name => "extrafooter");
+} # }}}
+
+sub mirrorlist ($) { #{{{
+	my $page=shift;
+	return "<p>Mirror".
+		(keys %{$config{mirrorlist}} > 1 ? "s" : "").
+		": ".
+		join(", ",
+			map { 
+				qq{<a href="}.
+				$config{mirrorlist}->{$_}."/".$page.
+				qq{">$_</a>}
+			} keys %{$config{mirrorlist}}
+		).
+		"</p>";
+} # }}}
+
+1
