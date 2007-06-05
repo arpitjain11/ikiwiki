435e06bce1fb8efe209ca0a7edf2c9f4d40db478
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2007-06-05 19:04:15 +0000

    * Add typographyattributes configuration setting to typography plugin.

--- IkiWiki/Plugin/typography.pm
+++ IkiWiki/Plugin/typography.pm
@@ -7,8 +7,16 @@ use strict;
 use IkiWiki 2.00;
 
 sub import { #{{{
+	hook(type => "getopt", id => "typography", call => \&getopt);
 	IkiWiki::hook(type => "sanitize", id => "typography", call => \&sanitize);
 } # }}}
+
+sub getopt () { #{{{
+	eval q{use Getopt::Long};
+	error($@) if $@;
+	Getopt::Long::Configure('pass_through');
+	GetOptions("typographyattributes=s" => \$config{typographyattributes});
+} #}}}
 
 sub sanitize (@) { #{{{
 	my %params=@_;
@@ -16,7 +24,8 @@ sub sanitize (@) { #{{{
 	eval q{use Text::Typography};
 	return $params{content} if $@;
 
-	return Text::Typography::typography($params{content});
+	my $attributes=defined $config{typographyattributes} ? $config{typographyattributes} : '3';
+	return Text::Typography::typography($params{content}, $attributes);
 } # }}}
 
 1
