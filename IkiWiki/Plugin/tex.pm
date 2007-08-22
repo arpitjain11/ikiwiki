f760fcfa3fe4660831f30651c203c3c9686c7bae
joey <joey@0fa5a96a-9a0e-0410-b3b2-a0fd24251071>
2007-08-22 00:59:41 +0000

    * Added tex plugin to generate images from latex code.   Contributed by Patrick Winnertz as a GSoC project.

---
+++ IkiWiki/Plugin/tex.pm
@@ -0,0 +1,181 @@
+#!/usr/bin/perl
+# Licensed under GPL v2 or greater
+# (c) 2007 Patrick Winnertz <patrick.winnertz@skolelinux.org>
+
+package IkiWiki::Plugin::tex;
+use warnings;
+use strict;
+use Digest::MD5 qw(md5_hex);
+use File::Temp qw(tempdir);
+use URI::Escape qw(uri_escape);
+use IkiWiki 2.00;
+
+sub import { #{{{
+	hook(type => "preprocess", id => "tex", call => \&preprocess);
+} #}}}
+
+sub preprocess (@) { #{{{
+	my %params = @_;
+	
+	my $height = $params{height};
+	if (! defined $height || ! length $height) {
+		$height = 12;
+	}
+	else {
+		$height =~ s#(\d+)#$1#;
+	}
+	
+	my $code = $params{code};
+	if (! defined $code && ! length $code) {
+		return "[[tex ".gettext("missing tex code"). "]]";
+	}
+
+	if (check($code)) {
+		return create($code, check_height($height), \%params);
+	}
+	else {
+		return "[[tex ".gettext("code includes disallowed latex commands"). "]]";
+	}
+} #}}}
+
+sub check_height ($) { #{{{
+	# Since latex doesn't support unlimited scaling this function
+	# returns the closest supported size.
+	my $height =shift;
+
+	my @allowed=(8,9,10,11,12,14,17,20);
+
+	my $ret;
+	my $fit;
+	foreach my $val (@allowed) {
+		my $f = abs($val - $height);
+		if (! defined($fit) || $f < $fit ) {
+			$ret=$val;
+			$fit=$f;
+		}
+	}
+	return $ret;
+} #}}}
+
+sub create ($$$) { #{{{
+	# This function calls the image generating function and returns
+	# the <img .. /> for the generated image.
+	my $code = shift;
+	my $height = shift;
+	my $params = shift;
+
+	if (! defined($height) and not length($height) ) {
+		$height = 12;
+	}
+
+	my $digest = md5_hex($code, $height);
+
+	my $teximgdir = "/teximages";
+	my $imglink = "$teximgdir/$digest.png";
+	my $imglog = "$teximgdir/$digest.log";
+	will_render($params->{destpage}, $imglink);
+	will_render($params->{destpage}, $imglog);
+
+	my $imgurl;
+	my $logurl;
+	if (! $params->{preview}) {
+		$imgurl = urlto($imglink, $params->{destpage});
+		$logurl = urlto($imglog, $params->{destpage});
+	}
+	else {
+		$imgurl="$config{url}/$teximgdir/$digest.png";
+		$logurl="$config{url}/$teximgdir/$digest.log";
+	}
+	
+	if (-e "$config{destdir}/$imglink" ||
+	    gen_image($code, $height, $digest, $teximgdir)) {
+		return qq{<img src="$imgurl" alt="}.uri_escape($code).qq{" class="teximage" />};
+	}
+	else {
+		return qq{[[tex <a href="$logurl">}.gettext("failed to generate image from code")."</a>]]";
+	}
+} #}}}
+
+sub gen_image ($$$$) { #{{{
+	# Actually creates the image.
+	my $code = shift;
+	my $height = shift;
+	my $digest = shift;
+	my $imagedir = shift;
+
+	#TODO This should move into the setup file.
+	my $tex = '\documentclass['.$height.'pt]{scrartcl}';
+	$tex .= '\usepackage[version=3]{mhchem}';
+	$tex .= '\usepackage{amsmath}';
+	$tex .= '\usepackage{amsfonts}';
+	$tex .= '\usepackage{amssymb}';
+	$tex .= '\pagestyle{empty}';
+	$tex .= '\begin{document}';
+	$tex .= '$$'.$code.'$$';
+	$tex .= '\end{document}';
+
+	my $tmp = eval { create_tmp_dir($digest) };
+	if (! $@ &&
+	    writefile("$digest.tex", $tmp, $tex) &&
+	    system("cd $tmp; latex --interaction=nonstopmode $tmp/$digest.tex > /dev/null") == 0 &&
+	    system("dvips -E $tmp/$digest.dvi -o $tmp/$digest.ps 2> $tmp/$digest.log") == 0 &&
+	    # ensure destination directory exists
+	    writefile("$imagedir/$digest.png", $config{destdir}, "") &&
+	    system("convert -density 120  -trim -transparent \"#FFFFFF\" $tmp/$digest.ps $config{destdir}/$imagedir/$digest.png > $tmp/$digest.log") == 0) {
+		return 1;
+	}
+	else {
+		# store failure log
+		my $log;
+		{
+			open(my $f, '<', "$tmp/$digest.log");
+			local $/=undef;
+			$log = <$f>;
+			close($f);
+		}
+		writefile("$digest.log", "$config{destdir}/$imagedir", $log);
+
+		return 0;
+	}
+} #}}}
+
+sub create_tmp_dir ($) { #{{{
+	# Create a temp directory, it will be removed when ikiwiki exits.
+	my $base = shift;
+
+	my $template = $base.".XXXXXXXXXX";
+	my $tmpdir = tempdir($template, TMPDIR => 1, CLEANUP => 1);
+	return $tmpdir;
+} #}}}
+
+sub check ($) { #{{{
+	# Check if the code is ok
+	my $code = shift;
+
+	my @badthings = (
+		qr/\$\$/,
+		qr/\\include/,
+		qr/\\includegraphic/,
+		qr/\\usepackage/,
+		qr/\\newcommand/, 
+		qr/\\renewcommand/,
+		qr/\\def/,
+		qr/\\input/,
+		qr/\\open/,
+		qr/\\loop/,
+		qr/\\errorstopmode/,
+		qr/\\scrollmode/,
+		qr/\\batchmode/,
+		qr/\\read/,
+		qr/\\write/,
+	);
+	
+	foreach my $thing (@badthings) {
+		if ($code =~ m/$thing/ ) {
+			return 0;
+		}
+	}
+	return 1;
+} #}}}
+
+1
