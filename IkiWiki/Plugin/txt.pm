a4d693f659b901557e596c41d3ab48bce804d7f6
Joey Hess <joey@kodama.kitenet.net>
2008-06-24 20:35:21 -0400

    formatting

--- IkiWiki/Plugin/txt.pm
+++ IkiWiki/Plugin/txt.pm
@@ -13,33 +13,34 @@ use HTML::Entities;
 require URI::Find;
 
 sub import {
-        hook(type => "filter",  id => "txt", call => \&filter);
-        hook(type => "htmlize", id => "txt", call => \&htmlize);
+	hook(type => "filter",  id => "txt", call => \&filter);
+	hook(type => "htmlize", id => "txt", call => \&htmlize);
 }
 
 # We use filter to convert raw text to HTML
 # (htmlize is called after other plugins insert HTML)
 sub filter (@) {
-    my %params = @_;
-    my $content = $params{content};
+	my %params = @_;
+	my $content = $params{content};
 
-    if ($pagesources{$params{page}} =~ /.txt$/) {
-        encode_entities($content);
-        my $finder = URI::Find->new(
-            sub {
-                my($uri, $orig_uri) = @_;
-                return qq|<a href="$uri">$orig_uri</a>|;
-            });
-        $finder->find(\$content);
-        $content = "<pre>" . $content . "</pre>";
-    }
-    return $content;
+	if ($pagesources{$params{page}} =~ /.txt$/) {
+		encode_entities($content);
+		my $finder = URI::Find->new(
+		sub {
+			my ($uri, $orig_uri) = @_;
+			return qq|<a href="$uri">$orig_uri</a>|;
+		});
+		$finder->find(\$content);
+		$content = "<pre>" . $content . "</pre>";
+	}
+
+	return $content;
 }
 
 # We need this to register the .txt file extension
 sub htmlize (@) {
-    my %params=@_;
-    return $params{content};
+	my %params=@_;
+	return $params{content};
 }
 
 1
