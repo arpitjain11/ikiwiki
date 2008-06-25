7fbbcc1615510b622e2d2947e07d9acbb56fbb1c
Joey Hess <joey@kodama.kitenet.net>
2008-06-24 20:38:41 -0400

    only convert urls if the module is installed

--- IkiWiki/Plugin/txt.pm
+++ IkiWiki/Plugin/txt.pm
@@ -10,11 +10,17 @@ use warnings;
 use strict;
 use IkiWiki 2.00;
 use HTML::Entities;
-require URI::Find;
+
+my $findurl=0;
 
 sub import {
 	hook(type => "filter",  id => "txt", call => \&filter);
 	hook(type => "htmlize", id => "txt", call => \&htmlize);
+
+	eval q{use URI::Find};
+	if (! $@) {
+		$findurl=1;
+	}
 }
 
 # We use filter to convert raw text to HTML
@@ -23,14 +29,15 @@ sub filter (@) {
 	my %params = @_;
 	my $content = $params{content};
 
-	if ($pagesources{$params{page}} =~ /.txt$/) {
+	if ($pagesources{$params{page}} =~ /\.txt$/) {
 		encode_entities($content);
-		my $finder = URI::Find->new(
-		sub {
-			my ($uri, $orig_uri) = @_;
-			return qq|<a href="$uri">$orig_uri</a>|;
-		});
-		$finder->find(\$content);
+		if ($findurl) {
+			my $finder = URI::Find->new(sub {
+				my ($uri, $orig_uri) = @_;
+				return qq|<a href="$uri">$orig_uri</a>|;
+			});
+			$finder->find(\$content);
+		}
 		$content = "<pre>" . $content . "</pre>";
 	}
 
