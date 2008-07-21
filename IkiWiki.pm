c2ffd205f3301a9fe6b5949a88da770899642589
Joey Hess <joey@kodama.kitenet.net>
2008-07-21 17:26:54 -0400

    Really fix bug with links to pages with names containing colons
    Previous fix mised a few cases.

--- IkiWiki.pm
+++ IkiWiki.pm
@@ -538,7 +538,7 @@ sub beautify_urlpath ($) { #{{{
 
 	# Ensure url is not an empty link, and
 	# if it's relative, make that explicit to avoid colon confusion.
-	if ($url !~ /\//) {
+	if ($url !~ /^\//) {
 		$url="./$url";
 	}
 
