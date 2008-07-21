af9c5e4bbadc87850f435cfc530ffc1d81419564
Joey Hess <joey@kodama.kitenet.net>
2008-07-21 15:02:29 -0400

    support for removing attachments

--- IkiWiki/Plugin/remove.pm
+++ IkiWiki/Plugin/remove.pm
@@ -24,17 +24,14 @@ sub formbuilder_setup (@) { #{{{
 	}
 } #}}}
 
-sub confirmation_form ($$$) { #{{{ 
+sub confirmation_form ($$) { #{{{ 
 	my $q=shift;
 	my $session=shift;
-	my $page=shift;
 
 	eval q{use CGI::FormBuilder};
 	error($@) if $@;
 	my @fields=qw(do page);
 	my $f = CGI::FormBuilder->new(
-		title => sprintf(gettext("confirm removal of %s"),
-			IkiWiki::pagetitle($page)),
 		name => "remove",
 		header => 0,
 		charset => "utf-8",
@@ -47,36 +44,61 @@ sub confirmation_form ($$$) { #{{{
 	);
 	
 	$f->field(name => "do", type => "hidden", value => "remove", force => 1);
-	$f->field(name => "page", type => "hidden", value => $page, force => 1);
 
 	return $f, ["Remove", "Cancel"];
 } #}}}
+
+sub removal_confirm ($$@) {
+	my $q=shift;
+	my $session=shift;
+	my $attachment=shift;
+	my @pages=@_;
+
+   	# Save current form state to allow returning to it later
+	# without losing any edits.
+	# (But don't save what button was submitted, to avoid
+	# looping back to here.)
+	# Note: "_submit" is CGI::FormBuilder internals.
+	$q->param(-name => "_submit", -value => "");
+	$session->param(postremove => scalar $q->Vars);
+	IkiWiki::cgi_savesession($session);
+	
+	my ($f, $buttons)=confirmation_form($q, $session);
+	$f->title(sprintf(gettext("confirm removal of %s"),
+		join(", ", map { IkiWiki::pagetitle($_) } @pages)));
+	$f->field(name => "page", type => "hidden", value => \@pages, force => 1);
+	if (defined $attachment) {
+		$f->field(name => "attachment", type => "hidden",
+			value => $attachment, force => 1);
+	}
+
+	IkiWiki::showform($f, $buttons, $session, $q);
+	exit 0;
+}
+
+sub postremove ($) {
+	my $session=shift;
+
+	# Load saved form state and return to edit form.
+	my $postremove=CGI->new($session->param("postremove"));
+	$session->clear("postremove");
+	IkiWiki::cgi_savesession($session);
+	IkiWiki::cgi($postremove, $session);
+}
 
 sub formbuilder (@) { #{{{
 	my %params=@_;
 	my $form=$params{form};
 
 	if (defined $form->field("do") && $form->field("do") eq "edit") {
-		if ($form->submitted eq "Remove") {
-			my $q=$params{cgi};
-			my $session=$params{session};
+		my $q=$params{cgi};
+		my $session=$params{session};
 
-		    	# Save current form state to allow returning to it later
-			# without losing any edits.
-			# (But don't save what button was submitted, to avoid
-			# looping back to here.)
-			# Note: "_submit" is CGI::FormBuilder internals.
-			$q->param(-name => "_submit", -value => "");
-			$session->param(postremove => scalar $q->Vars);
-			IkiWiki::cgi_savesession($session);
-	
-			# Display a small confirmation form.
-			my ($f, $buttons)=confirmation_form($q, $session, $form->field("page"));
-			IkiWiki::showform($f, $buttons, $session, $q);
-			exit 0;
+		if ($form->submitted eq "Remove") {
+			removal_confirm($q, $session, 0, $form->field("page"));
 		}
 		elsif ($form->submitted eq "Remove Attachments") {
-			
+			removal_confirm($q, $session, 1, $q->param("attachment_select"));
 		}
 	}
 } #}}}
@@ -86,49 +108,59 @@ sub sessioncgi ($$) { #{{{
 
 	if ($q->param("do") eq 'remove') {
         	my $session=shift;
-		my ($form, $buttons)=confirmation_form($q, $session, $session->param("page"));
+		my ($form, $buttons)=confirmation_form($q, $session);
 		IkiWiki::decode_form_utf8($form);
 
 		if ($form->submitted eq 'Cancel') {
-			# Load saved form state and return to edit form.
-			my $postremove=CGI->new($session->param("postremove"));
-			$session->clear("postremove");
-			IkiWiki::cgi_savesession($session);
-			IkiWiki::cgi($postremove, $session);
+			postremove($session);
 		}
 		elsif ($form->submitted eq 'Remove' && $form->validate) {
-			my $page=$form->field("page");
-			my $file=$pagesources{$page};
+			my @pages=$q->param("page");
+			my @files = map { $pagesources{$_} } @pages;
 	
 			# Validate removal by checking that the page exists,
 			# and that the user is allowed to edit(/remove) it.
-			if (! exists $pagesources{$page}) {
-				error(sprintf(gettext("%s does not exist"),
-				htmllink("", "", $page, noimageinline => 1)));
+			foreach my $page (@pages) {
+				if (! exists $pagesources{$page}) {
+					error(sprintf(gettext("%s does not exist"),
+					htmllink("", "", $page, noimageinline => 1)));
+				}
+				IkiWiki::check_canedit($page, $q, $session);
 			}
-			IkiWiki::check_canedit($page, $q, $session);
 
 			# Do removal, and update the wiki.
 			require IkiWiki::Render;
 			if ($config{rcs}) {
-				IkiWiki::rcs_remove($file);
 				IkiWiki::disable_commit_hook();
-				IkiWiki::rcs_commit($file, gettext("removed"),
-					IkiWiki::rcs_prepedit($file),
-					$session->param("name"), $ENV{REMOTE_ADDR});
+				foreach my $file (@files) {
+					IkiWiki::rcs_remove($file);
+					IkiWiki::rcs_commit($file, gettext("removed"),
+						IkiWiki::rcs_prepedit($file),
+						$session->param("name"), $ENV{REMOTE_ADDR});
+				}
 				IkiWiki::enable_commit_hook();
 				IkiWiki::rcs_update();
 			}
-			IkiWiki::prune("$config{srcdir}/$file");
+			foreach my $file (@files) {
+				IkiWiki::prune("$config{srcdir}/$file");
+			}
 			IkiWiki::refresh();
 			IkiWiki::saveindex();
 
-			# Redirect to parent of the page.
-			my $parent=IkiWiki::dirname($page);
-			if (! exists $pagesources{$parent}) {
-				$parent="index";
+			if ($q->param("attachment")) {
+				# Attachments were deleted, so redirect
+				# to the edit form.
+				postremove($session);
 			}
-			IkiWiki::redirect($q, $config{url}."/".htmlpage($parent));
+			else {
+				# The page is gone, so redirect to parent
+				# of the page.
+				my $parent=IkiWiki::dirname($pages[0]);
+				if (! exists $pagesources{$parent}) {
+					$parent="index";
+				}
+				IkiWiki::redirect($q, $config{url}."/".htmlpage($parent));
+			}
 		}
 		else {
 			IkiWiki::showform($form, $buttons, $session, $q);
