db146d9f1e70b69b13b02b7484027b288de5decf
Joey Hess <joey@kodama.kitenet.net>
2008-10-19 19:39:32 -0400

    need to use localtime, fix width

--- IkiWiki/Plugin/relativedate.pm
+++ IkiWiki/Plugin/relativedate.pm
@@ -47,9 +47,10 @@ sub IkiWiki::displaytime ($;$) { #{{{
 	my $format=shift;
 
 	# This needs to be in a form that can be parsed by javascript.
-	# Being fairly human readable is also nice, as it will be exposed as the title
-	# if javascript is not available.
-	my $gmtime=decode_utf8(POSIX::strftime("%a, %d %b %Y %H:%M:%S %z", gmtime($time)));
+	# Being fairly human readable is also nice, as it will be exposed
+	# as the title if javascript is not available.
+	my $gmtime=decode_utf8(POSIX::strftime("%a, %d %b %Y %H:%M:%S %z",
+			localtime($time)));
 
 	return '<span class="date" title="'.$gmtime.'">'.
 		IkiWiki::formattime($time, $format).'</span>';
