66bc27b485e6dd167d0394db18a078d5b7977ae3
Joey Hess <joey@gnu.kitenet.net>
2009-01-26 13:08:25 -0500

    avoid mixing english with translation
    "google search" is english which would get mixed in with translated text.
    Instead, usr just the plugin name.

--- IkiWiki/Plugin/google.pm
+++ IkiWiki/Plugin/google.pm
@@ -24,7 +24,7 @@ sub getsetup () {
 
 sub checkconfig () {
 	if (! length $config{url}) {
-		error(sprintf(gettext("Must specify %s when using the %s plugin"), "url", 'google search'));
+		error(sprintf(gettext("Must specify %s when using the %s plugin"), "url", 'google'));
 	}
 	my $uri=URI->new($config{url});
 	if (! $uri || ! defined $uri->host) {
