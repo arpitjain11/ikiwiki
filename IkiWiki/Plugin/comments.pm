4e92548ebc39c083e7ed8b870f4c13ba229127cd
Simon McVittie <smcv@ http://smcv.pseudorandom.co.uk/>
2009-01-31 14:49:12 +0000

    comments: delete cgi hook in favour of the global one

--- IkiWiki/Plugin/comments.pm
+++ IkiWiki/Plugin/comments.pm
@@ -25,7 +25,6 @@ sub import {
 	hook(type => "sessioncgi", id => 'comment', call => \&sessioncgi);
 	hook(type => "htmlize", id => "_comment", call => \&htmlize);
 	hook(type => "pagetemplate", id => "comments", call => \&pagetemplate);
-	hook(type => "cgi", id => "comments", call => \&linkcgi);
 	hook(type => "formbuilder_setup", id => "comments", call => \&formbuilder_setup);
 	IkiWiki::loadplugin("inline");
 }
@@ -167,7 +166,7 @@ sub preprocess {
 		}
 		else {
 			$commentauthorurl = IkiWiki::cgiurl(
-				do => 'commenter',
+				do => 'goto',
 				page => (length $config{userdir}
 					? "$config{userdir}/$commentuser"
 					: "$commentuser"));
@@ -233,35 +232,6 @@ sub preprocess {
 	}
 
 	return $content;
-}
-
-# This is exactly the same as recentchanges_link :-(
-sub linkcgi ($) {
-	my $cgi=shift;
-	if (defined $cgi->param('do') && $cgi->param('do') eq "commenter") {
-
-		my $page=decode_utf8($cgi->param("page"));
-		if (! defined $page) {
-			error("missing page parameter");
-		}
-
-		IkiWiki::loadindex();
-
-		my $link=bestlink("", $page);
-		if (! length $link) {
-			print "Content-type: text/html\n\n";
-			print IkiWiki::misctemplate(gettext(gettext("missing page")),
-				"<p>".
-				sprintf(gettext("The page %s does not exist."),
-					htmllink("", "", $page)).
-				"</p>");
-		}
-		else {
-			IkiWiki::redirect($cgi, urlto($link, undef, 1));
-		}
-
-		exit;
-	}
 }
 
 sub sessioncgi ($$) {
