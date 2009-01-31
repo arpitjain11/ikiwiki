3e290ce7eef9de6bad7d30b5855de14090b5edd2
Simon McVittie <smcv@ http://smcv.pseudorandom.co.uk/>
2009-01-31 18:06:44 +0000

    IkiWiki::Wrapper: allow REDIRECT_STATUS and REDIRECT_URL through from environment
    This is useful to act as an Apache 404 ErrorDocument.

--- IkiWiki/Wrapper.pm
+++ IkiWiki/Wrapper.pm
@@ -28,7 +28,8 @@ sub gen_wrapper () {
 	my @envsave;
 	push @envsave, qw{REMOTE_ADDR QUERY_STRING REQUEST_METHOD REQUEST_URI
 	               CONTENT_TYPE CONTENT_LENGTH GATEWAY_INTERFACE
-		       HTTP_COOKIE REMOTE_USER HTTPS} if $config{cgi};
+		       HTTP_COOKIE REMOTE_USER HTTPS REDIRECT_STATUS
+		       REDIRECT_URL} if $config{cgi};
 	my $envsave="";
 	foreach my $var (@envsave) {
 		$envsave.=<<"EOF";
